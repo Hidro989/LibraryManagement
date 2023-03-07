fde1ebaed5724b4a7d7bf1a794545ba7e5f42b2d
Hidro989 <huynguyenquang989@gmail.com>
2023-03-07 09:52:13 +0700

    Resolving conflicts

--- LibraryManagement/routes/web.php
+++ LibraryManagement/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\AdminController;
+use App\Http\Middleware\Authenticate;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\TypeBookController;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('admin.auth.login');
-// });
 
 Route::resource('book', BookController::class);
 Route::resource('type', TypeBookController::class);
+Route::middleware('isLoggedin')->group(function () {
+    Route::get("", [AdminController::class, 'index'])->name('dashboard');
+    Route::get("logout", [AdminController::class, 'logout'])->name('logout');
+});
+Route::get('login', [AdminController::class, 'showLogin'])->name('login')->middleware('alreadyLoggedIn');
+Route::post('login', [AdminController::class, 'login'])->name('login');
+Route::get('page404', [AdminController::class, 'page404'])->name('page404');
